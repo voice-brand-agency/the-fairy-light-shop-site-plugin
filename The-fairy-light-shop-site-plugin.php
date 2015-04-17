52391237542aee9fef64985e0fb6fee3823f4324
Nicholas Byfleet <nick@voice.co.nz>
2015-04-17 12:05:58 +1200

    Initial commit. In the middle of porting from WCPBC to TFLS.

---
+++ The-fairy-light-shop-site-plugin.php
@@ -0,0 +1,137 @@
+<?php
+
+/*
+Plugin Name: The Fairy Light Shop Site Plugin
+Plugin URI: http://voice.co.nz/
+Description: Our take on WooCommerce Product Price Based on Countries (https://github.com/wp-plugins/woocommerce-product-price-based-on-countries) for TFLS
+Version: 1.0
+Author: Nicholas Byfleet
+Author URI: http://nbyfleet.com/
+License: A "Slug" license name e.g. GPL2
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+if ( in_array( 'woocommerce/wooocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) : // Is woocommerce active?
+
+	if ( ! class_exists( 'WC_Fairy_Light_Shop' ) ) : // Make sure that we're not stepping on anyone's toes
+
+		/**
+		 * Main WC Fairy Light Shop Class
+		 *
+		 * @class WC_Fairy_Light_Shop
+		 * @version 1.0.0
+		 */
+		class WC_Fairy_Light_Shop {
+
+			/**
+			 * @var The single instance of the class
+			 */
+			protected $_instance;
+
+			/**
+			 * @var $regions
+			 */
+			protected $regions = null;
+
+			/**
+			 * Main WC_Fairy_Light_Shop Instance
+			 *
+			 * @static
+			 * @see TFLS()
+			 */
+			public static function instance() {
+
+				if ( is_null( self::$_instance ) ) {
+					self::$_instance = new self();
+				}
+
+				return self::$_instance;
+			}
+
+			public function __construct() {
+
+				$upload_dir = wp_upload_dir();
+
+				// I presume these will be used to help administer the monthly refresh of the GeoIP database
+				define( 'TFLS_FILE', __FILE__ );
+				define( 'TFLS_UPLOAD_DIR', $upload_dir['base_dir'] . '/the_fairy_light_shop_site_plugin' );
+				define( 'TFLS_GEOIP_DB', TFLS_UPLOAD_DIR . '/GeoLite2-Country.mmdb' );
+
+				include_once 'includes/tfls-functions.php';
+
+				if ( $this->is_reqest( 'admin' ) ) {
+					include_once 'includes/class-tfls-admin.php';
+				} elseif ( $this->is_request( 'frontend' ) ) {
+					require_once 'includes/class-tfls-frontend.php';
+				}
+			}
+
+			/**
+			 * Get regions
+			 * @return array
+			 */
+			public function get_regions() {
+
+				if ( is_null( $this->regions ) ) {
+					$regions = get_option( '_oga_tfls_countries_groups' );
+
+					if ( ! $regions ) {
+						$regions = array();
+					}
+
+					$this->regions = $regions;
+				}
+
+				return $this->regions;
+			}
+
+			/**
+			 * What type of request is this?
+			 *
+			 * @param string $type frontend or admin
+			 *
+			 * @return bool
+			 */
+			private function is_request( $type ) {
+
+				$is_ajax = defined( 'DOING_AJAX' ) && DOING_AJAX;
+
+				switch ( $type ) {
+
+					case 'admin' :
+						$ajax_allow_actions = array( 'woocommerce_add_variation' );
+
+						return ( is_admin() && ! $is_ajax ) || ( is_admin() && $is_ajax && isset( $_POST['action'] ) && in_array( $_POST['action'], $ajax_allow_actions ) );
+
+					case 'frontend' :
+						return ! $this->is_request( 'bot' ) && file_exists( TFLS_GEOIP_DB ) && ( ! is_admin() || ( is_admin() && $is_ajax ) ) && ! defined( 'DOING_CRON' );
+					case 'bot':
+						$user_agent = strtolower( $_SERVER['HTTP_USER_AGENT'] );
+
+						return preg_match( "/googlebot|adsbot|yahooseeker|yahoobot|msnbot|watchmouse|pingdom\.com|feedfetcher-google/", $user_agent );
+				}
+			}
+		} // End class
+
+		/**
+		 * Returns the main instance of WC_Fairy_Light_Shop so as to avoid the need to use globals.
+		 *
+		 * @return WC_Fairy_Light_Shop
+		 */
+		function TFLS() {
+
+			return WC_Fairy_Light_Shop::instance();
+		}
+
+		$wc_the_fairy_light_shop = TFLS();
+
+	endif; // ! class_exists ( 'WC_The_Fairy_Light_Shop' )
+
+else:
+
+
+
+endif; // End check if woocommerce is enabled
---
+++ includes/tfls-functions.php
@@ -0,0 +1,138 @@
+<?php
+
+require_once '../vendor/autoload.php';
+
+use GeoIp2\Database\Reader;
+
+/**
+ * WooCommerce functions for the Fairy Light Shop
+ *
+ * These are general functions available on both the front end and back end.
+ */
+
+// As always, we want to exit if someone attempts to access the script directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Return country IsoCode from IP address.
+ *
+ * @return string
+ */
+if ( ! function_exists( 'get_country_from_ip' ) ) {
+	function get_country_from_ip( $ip ) {
+
+		$isoCode = '';
+
+		try {
+			$reader  = new Reader( TFLS_GEOIP_DB );
+			$record  = $reader->country( $ip );
+			$isoCode = $record->country->isoCode;
+		} catch ( Exception $e ) {
+			error_log( $e->getMessage() );
+		}
+
+		return $isoCode;
+	}
+}
+
+/**
+ * Return country ISO code from client IP
+ *
+ * @return string
+ */
+if ( ! function_exists( 'country_from_client_ip' ) ) {
+
+	function country_from_client_ip() {
+
+		$debug_ip      = get_option( 'tfls_debug_ip' );
+		$debug_enabled = get_option( 'tfls_debug_mode' );
+
+		if ( $debug_enabled && ! empty( $debug_ip ) ) {
+			$client_ip = $debug_ip;
+		} else {
+			if ( isset( $_SERVER['HTTP_CLIENT_IP'] ) && $_SERVER['HTTP_CLIENT_IP'] ) {
+				$client_ip = $_SERVER['HTTP_CLIENT_IP'];
+			} elseif ( isset( $_SERVER['HTTP_X_FORWARDED_FOR'] ) && $_SERVER['HTTP_X_FORWARDED_FOR'] ) {
+				$client_ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+			} else {
+				$client_ip = $_SERVER['REMOTE_ADDR'];
+			}
+		}
+
+		return get_country_from_ip( $client_ip );
+	}
+}
+
+/**
+ * Download GeoIP Database
+ */
+if ( ! function_exists( 'tfls_download_geoipdb' ) ) {
+	function tfls_download_geoipdb() {
+
+		$result = '';
+
+		// We need the download_url() function, it should exist on virtually all installs of PHP, but if it doesn't for some
+		// reason, bail out.
+		if ( function_exists( 'download_url' ) ) {
+
+			// TODO: Remove hard coding of URL
+			$download_url = "http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz";
+
+			// Check to see if the subdirectory we're going to download to exists, if not, create it.
+			if ( ! file_exists( TFLS_UPLOAD_DIR ) ) {
+				wp_mkdir_p( TFLS_UPLOAD_DIR );
+			}
+
+			$tmpFile = download_url( $download_url );
+
+			if ( is_wp_error( $tmpFile ) ) {
+				$result = sprintf( __( 'Error downloading GeoIP database from: %s. %s', 'the-fairy-light-shop-site-plugin' ), $download_url, $tmpFile->get_error_message() );
+			} else {
+
+				// Gunzip file
+				$zh = gzopen( $tmpFile, 'rb' );
+				$h  = fopen( TFLS_GEOIP_DB, 'wb' );
+
+				// If we failed, display message
+				if ( ! $zh ) {
+					$result = __( 'Downloaded file could not be opened for reading.', 'the-fairy-light-shop-site-plugin' );
+				} elseif ( ! $h ) {
+
+					// woocommerce-product-price-based-on-countries had this:
+					// $result = sprintf(__('Database could not be written (%s).', 'woocommerce-product-price-based-countries'), $outFile);
+					//
+					// TODO: Was the use of $outFile an error? Or does this variable have special significance?
+					// TODO: I'm unable to find any reference to $outFile anywhere in the project source.
+
+					$result = __( 'Database could not be written (%s).', 'the-fairy-light-shop-site-plugin' );
+				} else {
+					// Read the database in 4kb chunks, writing to our local db.
+					while ( ( $string = gzread( $zh, 4096 ) ) !== false ) {
+						fwrite( $h, $string );
+					}
+
+					// Close all the handlers
+					gzclose( $zh );
+					fclose( $h );
+				}
+			}
+
+			unlink($tmpFile);
+
+		} // End function_exists( 'download_url' )
+
+		return $result;
+	}
+}
+
+// This basically maps the tfls_update_geoip hook to our tfls_download_geoipdb function
+add_action('tfls_update_geoip', 'tfls_download_geoipdb');
+
+/**
+ * Schedules regular updates of the geoip database.
+ *
+ * @param
+ */
+function tfls_cron_schedules($schedules)
